80c8ffc09523ca6c41412e7b9cf3b9236a011155
Jacky Zhao <jacky.zhao@bunchball.com>
2016-06-13 18:45:16 +0800

    basic new item component

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 * [LayoutAnimation](https://egghead.io/lessons/react-react-native-layoutanimation-basics)
 * [关于缓存](http://www.alloyteam.com/2016/03/best-practice-in-react-native/)
 * [Animated Picker](https://medium.com/@dabit3/creating-an-animated-picker-for-react-native-a0785ad5a39c#.a6dvnejoe)
+* [Keyboard](https://shift.infinite.red/avoiding-the-keyboard-in-react-native-56d05b9a1e81#.ooyfgligr)
--- app/components/NewItem.js
+++ app/components/NewItem.js
@@ -4,23 +4,73 @@ import React from 'react';
 import {
   StyleSheet,
   Text,
-  View
+  TextInput,
+  View,
+  Dimensions,
+  DeviceEventEmitter,
+  LayoutAnimation
 } from 'react-native';
 
 class NewItem extends React.Component {
   constructor(){
     super(...arguments);
     this.state = {
-      selectedTab: 'feed'
+      title: '',
+      content: '',
+      visibleHeight: 0
     };
   }
 
+  componentWillMount () {
+    this.keyboardDidShowListener = DeviceEventEmitter.addListener('keyboardDidShow', this.keyboardDidShow)
+    this.keyboardDidHideListener = DeviceEventEmitter.addListener('keyboardDidHide', this.keyboardDidHide)
+  }
+
+  componentWillUnmount () {
+    this.keyboardDidShowListener.remove()
+    this.keyboardDidHideListener.remove()
+  }
+
+  keyboardDidShow = (e) => {
+    LayoutAnimation.configureNext(LayoutAnimation.Presets.spring)
+    let visibleHeight = Dimensions.get('window').height - e.endCoordinates.height
+    console.log('keyboardDidShow');
+    console.log('visibleHeight ',visibleHeight);
+    this.setState({visibleHeight:visibleHeight})
+  }
+
+  keyboardDidHide = (e) => {
+    LayoutAnimation.configureNext(LayoutAnimation.Presets.spring)
+    console.log('keyboardDidHide');
+    console.log('visibleHeight ',Dimensions.get('window').height);
+    this.setState({
+      visibleHeight: Dimensions.get('window').height
+    })
+  }
+
   render(){
+    let bodyHeight = this.state.visibleHeight - 160;
+    console.log('bodyHeight ',bodyHeight);
     return (
       <View style={styles.container}>
-        <Text>
-        new item
-        </Text>
+         <View style={[styles.inputContainer]}>
+           <TextInput
+             autoFocus={true}
+             autoCapitalize="sentences"
+             placeholder="Untitled"
+             style={[styles.text,styles.title,{height:40}]}
+             onEndEditing={(text) => {this.refs.body.focus()}}
+           />
+         </View>
+         <View style={[styles.inputContainer]}>
+            <TextInput
+             ref="body"
+             multiline={true}
+             placeholder="Start typing"
+             style={[styles.text,{height:bodyHeight}]}
+             textAlignVertical="top"
+            />
+        </View>
       </View>
     );
   }
@@ -29,7 +79,20 @@ class NewItem extends React.Component {
 
 let styles = StyleSheet.create({
   container: {
-    flex: 1
+     flex: 1,
+     marginTop: 64,
+     marginLeft: 10,
+     marginRight: 10
+  },
+  inputContainer: {
+     borderBottomColor: 'gray',
+     borderBottomWidth: 1
+  },
+  text: {
+     fontSize: 20,
+  },
+  title: {
+    fontWeight: 'bold'
   }
 })
 
